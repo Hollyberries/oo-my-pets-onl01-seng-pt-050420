07563f6be7756e6b9b0e3bcec33a03916e247bc9
Holly Miranda-Brennecke <loyaltothesport85@gmail.com>
2020-06-08 16:20:18 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,17 +1,17 @@
 class Dog
-  
+  attr_accessor :mood, :owner
+  attr_reader :name
+
   @@all = []
-  
-  attr_accessor :name, :owner
-  
+
   def initialize(name, owner)
     @name = name
+    @mood = "nervous"
     @owner = owner
+    @@all << self
   end
-  
+
   def self.all
     @@all
   end
-  
-  
 end
