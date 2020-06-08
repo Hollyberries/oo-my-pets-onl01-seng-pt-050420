32ffb1316501669332e8b1aa7c2839dea78fda06
Holly Miranda-Brennecke <loyaltothesport85@gmail.com>
2020-06-08 16:19:44 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,14 +1,17 @@
 class Cat
-  
+  # code goes here
+  attr_accessor :mood, :owner
+  attr_reader :name
+
   @@all = []
-  
-  attr_accessor :name, :owner
-  
+
   def initialize(name, owner)
     @name = name
+    @mood = "nervous"
     @owner = owner
+    @@all << self
   end
-  
+
   def self.all
     @@all
   end
