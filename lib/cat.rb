f41ec579782f49742a693e55e11664bfa5714bf0
Holly Miranda-Brennecke <loyaltothesport85@gmail.com>
2020-06-08 16:16:38 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,10 +1,16 @@
 class Cat
   
+  @@all = []
+  
   attr_accessor :name, :owner
   
   def initialize(name, owner)
     @name = name
     @owner = owner
+  end
+  
+  def self.all
+    @@all
   end
   
 end
